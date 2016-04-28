6a3cad3233f03c878c60784477426f5cbf345324
Charles Peach <me@charlespeach.com>
2016-04-29 09:25:21 +1200

    Move all of sublimes package control and config to dotfiles.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.irbrc ~/.irbrc
-ln -s ~/.dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -s ~/.dotfiles/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 #Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 0.02
@@ -19,3 +19,27 @@ chflags nohidden ~/Library
 #Store screenshots in subfolder on desktop
 mkdir ~/Desktop/Screenshots
 defaults write com.apple.screencapture location ~/Desktop/Screenshots
+
+
+# MySQL
+brew install mysql
+brew pin mysql
+
+# MySQL Settings
+# Copy launch agent into place
+mkdir -p ~/Library/LaunchAgents && cp /usr/local/Cellar/mysql/5.7.12/homebrew.mxcl.mysql.plist ~/Library/LaunchAgents/
+
+# Edit launch agent and set both keepalive and launch at startup to false
+vi ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+
+# Inject launch agent
+launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+
+# Set up databases to run as your user account
+unset TMPDIR && mysqld --initialize --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
+
+# Start mysql
+start mysql
+
+# Secure mysql
+/usr/local/Cellar/mysql/5.7.12/bin/mysql_secure_installation
