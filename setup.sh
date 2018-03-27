6306111e78c133d618497c14a4b84acbbf0d823f
Charles Peach <me@charlespeach.com>
2018-03-27 17:55:27 +1300

    Update setup script. Make it executable and remove mysql steps

--- setup.sh
+++ setup.sh
@@ -1,48 +1,25 @@
+#!/bin/bash
+
 # Symlink all the things
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.irbrc ~/.irbrc
 ln -s ~/.dotfiles/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
-#Set a blazingly fast keyboard repeat rate
+# Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 0.02
 
-#Set a shorter Delay until key repeat
+# Set a shorter Delay until key repeat
 defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
-#Add a context menu item for showing the Web Inspector in web views
+# Add a context menu item for showing the Web Inspector in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
-#Show the ~/Library folder
+# Show the ~/Library folder
 chflags nohidden ~/Library
 
-#Store screenshots in subfolder on desktop
+# Store screenshots in subfolder on desktop
 mkdir ~/Desktop/Screenshots
 defaults write com.apple.screencapture location ~/Desktop/Screenshots
-
-
-# MySQL
-brew install mysql
-brew pin mysql
-
-# MySQL Settings
-# Copy launch agent into place
-mkdir -p ~/Library/LaunchAgents && cp /usr/local/Cellar/mysql/5.7.12/homebrew.mxcl.mysql.plist ~/Library/LaunchAgents/
-
-# Edit launch agent and set both keepalive and launch at startup to false
-vi ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
-
-# Inject launch agent
-launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
-
-# Set up databases to run as your user account
-unset TMPDIR && mysqld --initialize --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
-
-# Start mysql
-start mysql
-
-# Secure mysql
-/usr/local/Cellar/mysql/5.7.12/bin/mysql_secure_installation
-
 
 ## Some extra stuff (DNS Masq etc) from here https://mallinson.ca/osx-web-development/
